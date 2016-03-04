b82f205736f42453bc331262e4a2168302cc4bb9
qq610540622 <610540622@qq.com>
2016-03-04 20:27:04 +0800

    v1.0.2

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ module.exports = function(app) {
                 res.redirect("/login");
             }
         }
+        next();
     })
 
     //　**********************　　前台　********************
