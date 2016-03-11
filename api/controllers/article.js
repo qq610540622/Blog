a3a61d4f58e91482364c9906ba78155fc1c83b2d
qq610540622 <610540622@qq.com>
2016-03-11 18:02:59 +0800

    v1.0.7

--- api/controllers/article.js
+++ api/controllers/article.js
@@ -13,7 +13,7 @@ var controller = {};
  * 获取当个或多个文章
  * ---------------------------
  * method：get
- * url:"http://localhost:3000/api/v2.1/article/get?id=id1"
+ * url:"http://localhost:3000/api/v1.0/article/get?id=id1"
  * 单个：　id='id1'
  * 多个：  id:'id1,id2,id3' 最多获取15条数据
  */
@@ -40,7 +40,7 @@ controller.get = function(req, res) {
  *  获取文章列表
  * ---------------------------
  * method：get
- * url:"http://localhost:3000/api/v2.1/article/list?page=1&rows=10"
+ * url:"http://localhost:3000/api/v1.0/article/list?page=1&rows=10"
  * page:当前页
  * rows:每页取多少条
  * @return total:总条数，rows:取出条数
@@ -64,7 +64,7 @@ controller.list = function(req, res) {
  * 删除
  * ---------------------------
  * method：post
- * url:"http://localhost:3000/api/v2.1/article/remove"
+ * url:"http://localhost:3000/api/v1.0/article/remove"
  * 单个：　data:{id:'id1'}
  * 多个：  data:{id:'id1,id2,id3'} 最多删除15条数据
  */
@@ -91,7 +91,7 @@ controller.remove = function(req,res) {
  * 创建
  * ---------------------------
  * method：post
- * url:"http://localhost:3000/api/v2.1/article/create"
+ * url:"http://localhost:3000/api/v1.0/article/create"
  * 单个：　data:{forumId:"*必填",title:"*必填",content:"可选",author:"可选",redirectUrl:"可选",tag:[可选],createDate:可选,readCount:可选}
  * 多个：  data:{list:数组} 最多删除15条数据
  */
@@ -137,7 +137,11 @@ controller.create = function(req,res) {
 
 /**
  * 修改
- * 
+ * ---------------------------
+ * method：post
+ * url:"http://localhost:3000/api/v1.0/article/update"
+ * 单个：　data:{_id:"必填"forumId:"*必填",title:"*必填",content:"可选",author:"可选",redirectUrl:"可选",tag:[可选],createDate:可选,readCount:可选}
+ * 多个：  data:{list:数组} 最多删除15条数据
  */
 controller.update = function(req,res) {
     if(req.body._id && req.body.forumId && req.body.title) {
@@ -158,6 +162,8 @@ controller.update = function(req,res) {
         });
     } else if(req.body.list) {
         var list = req.body.list;
+        if(list.length>15) common.resError(104,null,res);
+
         var isOk = true;
         for(var i=0,len=list.length; i<len; i++) {
             if(!list[i]._id || !list[i].forumId || !list[i].title) {
@@ -165,8 +171,8 @@ controller.update = function(req,res) {
                 break;
             }
         }
+
         if(isOk) {
-            console.log(list);
             list.forEach(function(item) {
                 articleDao.base.update({_id:item._id}, item, {upsert:false, multi:false}, function(err) {});
             })
