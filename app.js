068abbb2404d8f421e16863e4b7d583be6e5ca28
qq610540622 <610540622@qq.com>
2016-03-30 14:18:48 +0800

    v1.1.7

--- app.js
+++ app.js
@@ -15,157 +15,55 @@ var _ = require('lodash');
 var loader = require('loader');
 var csurf = require('csurf');
 
-//
-//if (cluster.isMaster) {
-//    var cpuCount = require('os').cpus().length;
-//    for (var i = 0; i < cpuCount; i += 1) {
-//        cluster.fork();
-//    }
-//    cluster.on('exit', function (worker) {
-//        console.log('Worker %d died :(', worker.id);
-//        cluster.fork();
-//    });
-//} else {
-//
-//    var app = express();
-//
-//    //视图引擎
-//    app.set('views', ['./app/show/views/pages','./app/admin/views']);
-//    app.set('view engine', 'html');
-//    app.engine('html', require('ejs-mate'));
-//    app.enable('trust proxy');
-//
-//    //指定静态资源
-//    app.use(express.static(path.join(__dirname, 'public')));
-//    _.extend(app.locals, {
-//        config: config,
-//        loader: loader
-//    });
-//
-//    //http请求体中间件
-//    app.use(bodyParser.json());
-//    app.use(bodyParser.urlencoded({ limit: '50mb',extended: false }));
-//
-//    //cookie,session中间件
-//    app.use(cookieParser(config.session_secret));
-//    //redis保存session
-//    app.use(session({
-//        secret: config.session_secret,
-//        store: new RedisStore({
-//            port: config.redis_port,
-//            host: config.redis_host
-//        }),
-//        resave: true,
-//        saveUninitialized: true
-//    }));
-//
-//    //自定义中间件
-//    app.use(auth.authPermission);
-//
-//
-//    //csrf
-//    app.use(function (req, res, next) {
-//        if (req.path.indexOf('/api') === -1 && req.path.indexOf('/admin') === -1) {
-//            csurf()(req, res, next);
-//            return;
-//        }
-//        next();
-//    });
-//    app.use(function(req, res, next) {
-//        res.locals.csrf = req.csrfToken ? req.csrfToken() : '';
-//        next();
-//    });
-//
-//    //路由
-//    var route = require('./routes/index');
-//    new route(app);
-//
-//    // error handler
-//    if (config.debug) {
-//        app.use(errorhandler());
-//    } else {
-//        app.use(function (err, req, res, next) {
-//            logger.error(err);
-//            return res.render("error");
-//        });
-//    }
-//
-//    //在线用户
-//    var onlineUsers = {};
-//    //当前在线人数
-//    var onlineCount = 0;
-//    var server = http.createServer(app);
-//    var io = require('socket.io').listen(server);
-//    io.on('connection', function(socket){
-//        socket.on('login', function(obj){
-//            console.log("login");
-//            var result = {};
-//            socket.name = obj.userid;
-//            if(!onlineUsers.hasOwnProperty(obj.userid)) {
-//                onlineUsers[obj.userid] = obj.username;
-//                onlineCount++;
-//            }
-//            result.onlineUsers = onlineUsers;
-//            result.onlineCount = onlineCount;
-//            result.user = obj;
-//            io.emit('login', result);
-//        });
-//
-//        socket.on('message', function(obj){
-//            io.emit('message', obj);
-//        });
-//
-//        socket.on('disconnect', function(){
-//            if(onlineUsers.hasOwnProperty(socket.name)) {
-//                var obj = {userid:socket.name, username:onlineUsers[socket.name]};
-//                delete onlineUsers[socket.name];
-//                onlineCount--;
-//                io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
-//            }
-//        });
-//    });
-//
-//    server.listen(config.port);
-//    console.log("server listen to port:%s",config.port);
-//}
-var app = express();
 
-//视图引擎
-app.set('views', ['./app/show/views/pages','./app/admin/views']);
-app.set('view engine', 'html');
-app.engine('html', require('ejs-mate'));
-app.enable('trust proxy');
+if (cluster.isMaster) {
+    var cpuCount = require('os').cpus().length;
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+    cluster.on('exit', function (worker) {
+        console.log('Worker %d died :(', worker.id);
+        cluster.fork();
+    });
+} else {
 
-//指定静态资源
-app.use(express.static(path.join(__dirname, 'public')));
-_.extend(app.locals, {
-    config: config,
-    loader: loader
-});
+    var app = express();
 
-//http请求体中间件
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ limit: '50mb',extended: false }));
+    //视图引擎
+    app.set('views', ['./app/show/views/pages','./app/admin/views']);
+    app.set('view engine', 'html');
+    app.engine('html', require('ejs-mate'));
+    app.enable('trust proxy');
 
-//cookie,session中间件
-app.use(cookieParser(config.session_secret));
-//redis保存session
-app.use(session({
-    secret: config.session_secret,
-    store: new RedisStore({
-        port: config.redis_port,
-        host: config.redis_host
-    }),
-    resave: true,
-    saveUninitialized: true
-}));
+    //指定静态资源
+    app.use(express.static(path.join(__dirname, 'public')));
+    _.extend(app.locals, {
+        config: config,
+        loader: loader
+    });
 
-//自定义中间件
-app.use(auth.authPermission);
+    //http请求体中间件
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ limit: '50mb',extended: false }));
+
+    //cookie,session中间件
+    app.use(cookieParser(config.session_secret));
+    //redis保存session
+    app.use(session({
+        secret: config.session_secret,
+        store: new RedisStore({
+            port: config.redis_port,
+            host: config.redis_host
+        }),
+        resave: true,
+        saveUninitialized: true
+    }));
+
+    //自定义中间件
+    app.use(auth.authPermission);
 
 
-//csrf
-if(!config.debug) {
+    //csrf
     app.use(function (req, res, next) {
         if (req.path.indexOf('/api') === -1 && req.path.indexOf('/admin') === -1) {
             csurf()(req, res, next);
@@ -173,64 +71,62 @@ if(!config.debug) {
         }
         next();
     });
-    app.set('view cache', true);
+    app.use(function(req, res, next) {
+        res.locals.csrf = req.csrfToken ? req.csrfToken() : '';
+        next();
+    });
+
+    //路由
+    var route = require('./routes/index');
+    new route(app);
+
+    // error handler
+    if (config.debug) {
+        app.use(errorhandler());
+    } else {
+        app.use(function (err, req, res, next) {
+            logger.error(err);
+            return res.render("error");
+        });
+    }
+
+    //在线用户
+    var onlineUsers = {};
+    //当前在线人数
+    var onlineCount = 0;
+    var server = http.createServer(app);
+    var io = require('socket.io').listen(server);
+    io.on('connection', function(socket){
+        socket.on('login', function(obj){
+            console.log("login");
+            var result = {};
+            socket.name = obj.userid;
+            if(!onlineUsers.hasOwnProperty(obj.userid)) {
+                onlineUsers[obj.userid] = obj.username;
+                onlineCount++;
+            }
+            result.onlineUsers = onlineUsers;
+            result.onlineCount = onlineCount;
+            result.user = obj;
+            io.emit('login', result);
+        });
+
+        socket.on('message', function(obj){
+            io.emit('message', obj);
+        });
+
+        socket.on('disconnect', function(){
+            if(onlineUsers.hasOwnProperty(socket.name)) {
+                var obj = {userid:socket.name, username:onlineUsers[socket.name]};
+                delete onlineUsers[socket.name];
+                onlineCount--;
+                io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
+            }
+        });
+    });
+
+    server.listen(config.port);
+    console.log("server listen to port:%s",config.port);
 }
-
-app.use(function(req, res, next) {
-    res.locals.csrf = req.csrfToken ? req.csrfToken() : '';
-    next();
-});
-
-//路由
-var route = require('./routes/index');
-new route(app);
-
-// error handler
-if (config.debug) {
-    app.use(errorhandler());
-} else {
-    app.use(function (err, req, res, next) {
-        logger.error(err);
-        return res.render("error");
-    });
-}
-
-//在线用户
-var onlineUsers = {};
-//当前在线人数
-var onlineCount = 0;
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-io.on('connection', function(socket){
-    socket.on('login', function(obj){
-        console.log("login");
-        var result = {};
-        socket.name = obj.userid;
-        if(!onlineUsers.hasOwnProperty(obj.userid)) {
-            onlineUsers[obj.userid] = obj.username;
-            onlineCount++;
-        }
-        result.onlineUsers = onlineUsers;
-        result.onlineCount = onlineCount;
-        result.user = obj;
-        io.emit('login', result);
-    });
-
-    socket.on('message', function(obj){
-        io.emit('message', obj);
-    });
-
-    socket.on('disconnect', function(){
-        if(onlineUsers.hasOwnProperty(socket.name)) {
-            var obj = {userid:socket.name, username:onlineUsers[socket.name]};
-            delete onlineUsers[socket.name];
-            onlineCount--;
-            io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
-        }
-    });
-});
-
-server.listen(config.port);
-console.log("server listen to port:%s",config.port);
 
 module.exports = app;
